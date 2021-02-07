064c5384cf44387713a5257daf127aeac7ba11cf
DiyaRahman <drahman024@gmail.com>
2021-02-07 13:49:28 +1030

    tasks

---
+++ LAB3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            int even = 0, odd = 0;
+
+            for (int i = 0; i <= num; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    even += i;
+                }
+                else
+                {
+                    odd += i;
+                }
+
+            }
+
+            Console.WriteLine(even);
+            Console.WriteLine(odd);
+
+            Console.ReadKey();
+        }
+        }
+    }
+
