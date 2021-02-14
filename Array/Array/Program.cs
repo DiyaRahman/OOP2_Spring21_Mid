aad175b74c9857c247fe260a3b3a6553f1a79fa3
DiyaRahman <drahman024@gmail.com>
2021-02-14 14:10:48 +1030

    Copy Array

---
+++ Array/Array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 1, 2 };
+            int[] arr2 = new int[arr1.Length];
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr2[i] = arr1[i];
+            }
+            Console.WriteLine(" array: ");
+            for(int i = 0; i < arr1.Length; i++)
+            { 
+            Console.Write(arr1[i] + " ");
+            }
+        
+            Console.WriteLine();
+
+            Console.WriteLine("new arry: ");
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.Write(arr2[i] + " ");
+            }
+        Console.ReadKey();
+        }
+
+    }
+}
