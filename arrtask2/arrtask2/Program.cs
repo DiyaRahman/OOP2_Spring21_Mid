62f55e7c0511f9581ca4eface32faaa003aaa8a0
DiyaRahman <drahman024@gmail.com>
2021-02-14 16:30:20 +1030

    array task2

---
+++ arrtask2/arrtask2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrtask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 1, 2, 3, 4 };
+            int i, j, duplicate = 0;
+
+            for (i = 0; i < arr1.Length; i++)
+            {
+                for (j = i + 1; j < arr1.Length; j++)
+                {
+                    if (arr1[i] == arr1[j]) ;
+                }
+            }
+            Console.WriteLine("Duplicate Number: "+duplicate);
+            Console.ReadKey();
+
+        }
+    }
+}
