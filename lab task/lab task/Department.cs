1c7719671f789f2958cb8263864f219519f3e4ec
DiyaRahman <drahman024@gmail.com>
2021-02-28 14:26:44 +1030

    lab task 28feb

---
+++ lab task/lab task/Department.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_task
+{
+    class Department
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string id;
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private Student[] student;
+        private int studentCount;
+        public int StudentCount
+        {
+            get { return studentCount; }
+            set { studentCount = value; }
+        }
+        public Department()
+        {
+            student = new Student[60];
+        }
+        public Department (string name,string id)
+        {
+            this.name = name;
+            this.id = id;
+            student = new Student[60];
+        }
+        public void AddStudent( Student student)
+        {
+            //student[studentCount++] = student;
+        }
+        public void AllStudents()
+        {
+            for (int i = 0; i < studentCount; i++)
+            {
+                student[i].ShowInfo();
+            }
+        }
+        public Student GetStudent(string id)
+        {
+            return null;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Id:" + id);
+        }
+
+
+    }
+}
---
+++ lab task/lab task/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department d1 = new Department("Computer Science", "Cse");
+            Student s1 = new Student("Diya", "18-38282-2", 3.50f);
+            d1.AllStudents();
+
+            s1.Department = d1;
+            s1.ShowInfo();
+
+
+        }
+    }
+}
