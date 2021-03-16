d88c67d7813d26f6da526f6cf7dca215a68856e8
DiyaRahman <drahman024@gmail.com>
2021-03-16 11:54:17 +1030

    abstract

---
+++ abstract/abstract/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @abstract
+{
+    class Account
+    {
+        public string AccName { get; set; }
+        public string AccNo { get; set; }
+        public string Balance { get; set; }
+
+        public Account(){}
+        public Account( string accName,string accNo,double balance) {
+
+            AccName = accName;
+            AccNo = accNo;
+            Balance = balance;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: " +AccName);
+            Console.WriteLine("Acc No: " + AccNo);
+            Console.WriteLine("Balance: " + Balance);
+        }
+    }
+}
